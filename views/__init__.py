2536a45885a1a709b1edf974a18577a499cb8c3a
Allison Blumenthal <blumen.allison@gmail.com>
2023-04-18 18:36:50 -0400

    added location requests

--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,5 @@
 from .animal_requests import get_all_animals
+from .animal_requests import get_single_animal
+
+from .location_requests import get_all_locations
+from .location_requests import get_single_location
