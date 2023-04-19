7ab6393d213cb23296f3a167db70b38c3e82b493
Allison Blumenthal <blumen.allison@gmail.com>
2023-04-18 20:12:59 -0400

    working through chapter 4

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import get_all_animals, get_single_animal, create_animal, get_all_locations, get_single_location, create_location, get_all_employees, get_single_employee, create_employee, get_all_customers, get_single_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -98,7 +98,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             else:
                 response = get_all_customers()
                 
-        # AB: What does wfile stand for?
+        # AB: What does wfile stand for? Is "w" write?
         self.wfile.write(json.dumps(response).encode())
       
 
@@ -106,20 +106,44 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any POST request.
     # AB: Why does it care about content length?
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
+        #What does rfile stand for? Is "r" read?
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_location = None
+        new_employee = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_animal).encode())
+        
+        if resource == "locations":
+            new_location = create_location(post_body)
+            
+        self.wfile.write(json.dumps(new_location).encode())
+        
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+            
+        self.wfile.write(json.dumps(new_employee).encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
     # AB: Is this just writing over what was set in do_POST?
-
     def do_PUT(self):
         """Handles PUT requests to the server
         """
